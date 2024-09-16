ec8ad51df9b1110558113d475fe8121f0b2a9391
Robson <robalbuquerque98@gmail.com>
2024-09-16 10:47:28 -0300

    add global styles and import in main.js

--- gerenciamentoSalas/src/main.js
+++ gerenciamentoSalas/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// Importa os estilos globais
+import './assets/styles/global.css';
+
 createApp(App)
     .use(router)
     .mount('#app')
