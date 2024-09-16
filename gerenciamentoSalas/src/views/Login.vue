1d5caa1eb8350a52e6ea64d2360bed23926ba6e0
Robson <robalbuquerque98@gmail.com>
2024-09-16 10:47:12 -0300

    feat: add Login and LoginForm components

---
+++ gerenciamentoSalas/src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login-page">
+    <h1>Login</h1>
+    <LoginForm />
+  </div>
+</template>
+
+<script setup>
+import LoginForm from '../components/LoginForm.vue';
+</script>
+
+<style scoped>
+/* Estilos específicos para o componente Login.vue */
+.login-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f0f2f5;
+}
+
+h1 {
+  font-size: 2rem;
+  color: #333;
+}
+</style>
