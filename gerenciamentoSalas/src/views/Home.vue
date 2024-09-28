2a53f15a47c8e19c68a19ab75c1ec33ab1950ec6
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-09-28 17:47:34 -0300

    chore: atualizações gerais de dependências e melhorias na Home.vue

--- gerenciamentoSalas/src/views/Home.vue
+++ gerenciamentoSalas/src/views/Home.vue
@@ -5,49 +5,44 @@
     <main>
       <CardList />
     </main>
-    <footer>
-      <p>Rodapé com links para redes sociais.</p>
-    </footer>
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from '../components/Header.vue';
-import Sidebar from '../components/Sidebar.vue';
-import CardList from '../components/CardList.vue';
+import Header from "../components/Header.vue";
+import Sidebar from "../components/Sidebar.vue";
+import CardList from "../components/CardList.vue";
+import Footer from "../components/Footer.vue";
 
 export default {
   components: {
     Header,
     Sidebar,
-    CardList
+    CardList,
+    Footer, // Importa o componente Footer
   },
   data() {
     return {
-      isSidebarActive: false
+      isSidebarActive: false,
     };
   },
   methods: {
     toggleSidebar() {
       this.isSidebarActive = !this.isSidebarActive;
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
 .container {
   display: flex;
-  flex-direction: column; /* Faz com que os elementos fiquem um abaixo do outro */
-  min-height: 100vh; /* Garante que a altura da página ocupe a tela inteira */
+  flex-direction: column;
+  min-height: 100vh;
 }
 main {
-  flex: 1; /* Ocupar o espaço restante */
-}
-footer {
-  text-align: center;
-  padding: 10px;
-  background-color: #4caf50;
-  color: white;
+  flex: 1;
+  padding: 20px;
 }
 </style>
