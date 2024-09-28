a27f9f01a4afdd171c81015430aa1f90f80a3de6
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-09-28 17:19:34 -0300

    chore: atualizações gerais de dependências e melhorias na Home.vue

--- gerenciamentoSalas/src/main.js
+++ gerenciamentoSalas/src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+
 // Importa os estilos globais
 import './assets/styles/global.css';
 
+// Importando o Font Awesome
+import '@fortawesome/fontawesome-free/css/all.css';
 createApp(App)
     .use(router)
     .mount('#app')
