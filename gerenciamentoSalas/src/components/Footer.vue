8bf53cd4d902bbebd24f054c61c9d7f4dc27df5e
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-09-28 17:25:09 -0300

    feat: adiciona componente Footer.vue

---
+++ gerenciamentoSalas/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    <p>{{ currentYear }} &copy; Todos os direitos reservados.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {
+      currentYear: new Date().getFullYear(), // Obter o ano atual
+    };
+  },
+};
+</script>
+
+<style scoped>
+footer {
+  text-align: center;
+  padding: 10px;
+  background-color: #4caf50;
+  color: white;
+  position: relative;
+  clear: both;
+}
+</style>
