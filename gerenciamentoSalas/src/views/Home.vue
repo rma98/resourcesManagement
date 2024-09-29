7f3d63c439c294f0f2f7386b17a90f8bc75b2a69
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-09-29 00:43:15 -0300

    chore: atualizações gerais de dependências e melhorias na Home.vue

--- gerenciamentoSalas/src/views/Home.vue
+++ gerenciamentoSalas/src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="container">
     <Header @toggle-sidebar="toggleSidebar" />
-    <Sidebar :isActive="isSidebarActive" />
+    <Sidebar
+      :isActive="isSidebarActive"
+      :isLoggedIn="isLoggedIn" 
+      @show-popup="showPopup" 
+      @close-sidebar="closeSidebar" 
+      @logout="logout" 
+    />
     <main>
       <CardList :spaces="spaces" @card-clicked="handleCardClick" />
     </main>
@@ -40,19 +46,30 @@ export default {
     toggleSidebar() {
       this.isSidebarActive = !this.isSidebarActive;
     },
+    closeSidebar() {
+      this.isSidebarActive = false; // Fecha a sidebar
+    },
+    showPopup() {
+      this.showLoginPopup = true; // Mostra o popup de login
+    },
     closePopup() {
       this.showLoginPopup = false; // Fecha o popup
     },
     handleCardClick(card) {
       console.log("Card clicado: ", card); // Verifica se o clique no card está funcionando
       if (!this.isLoggedIn) {
-        this.showLoginPopup = true; // Abre o popup de login se o usuário não estiver logado
+        this.showPopup(); // Abre o popup de login se o usuário não estiver logado
       } else {
         console.log(`Espaço reservado: ${card.title}`);
       }
     },
+    logout() {
+      this.isLoggedIn = false; // Simula logout
+      this.closeSidebar(); // Fecha a sidebar após logout
+    },
     redirectTo(page) {
       this.$router.push(page);
+      this.closeSidebar(); // Fecha a sidebar após redirecionar
     }
   }
 };
