94b122d51575ac086d82ee8416c430dd97a66052
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-10-25 17:40:12 -0300

    feat: add MainLayout.vue

--- gerenciamentoSalas/src/layouts/MainLayout.vue
+++ gerenciamentoSalas/src/layouts/MainLayout.vue
@@ -6,20 +6,29 @@
       <router-view></router-view>
       <!-- Aqui as páginas serão renderizadas -->
     </main>
+<<<<<<< HEAD
     <Footer />
+=======
+>>>>>>> bb3adb8 (feat: add MainLayout.vue)
   </div>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
 import Sidebar from "../components/Sidebar.vue";
+<<<<<<< HEAD
 import Footer from '../components/Footer.vue';
+=======
+>>>>>>> bb3adb8 (feat: add MainLayout.vue)
 
 export default {
   components: {
     Header,
     Sidebar,
+<<<<<<< HEAD
     Footer,
+=======
+>>>>>>> bb3adb8 (feat: add MainLayout.vue)
   },
   data() {
     return {
@@ -41,6 +50,7 @@ export default {
 .main-layout {
   display: flex;
   flex-direction: column;
+<<<<<<< HEAD
   min-height: 100vh;
   /* Define o mínimo como altura total da tela */
 }
@@ -48,5 +58,7 @@ export default {
 main {
   flex-grow: 1;
   /* Faz com que o conteúdo principal ocupe o espaço disponível */
+=======
+>>>>>>> bb3adb8 (feat: add MainLayout.vue)
 }
 </style>
