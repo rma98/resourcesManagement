ec244c0fd86c92e3aaa8e4b0b504d754f4e0aff1
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-10-25 17:35:19 -0300

    feat: update Header.vue

--- gerenciamentoSalas/src/components/Header.vue
+++ gerenciamentoSalas/src/components/Header.vue
@@ -1,27 +1,21 @@
 <template>
   <header>
     <h1>
-      <i class="fas fa-cog"></i>
-      Gerenciamento de Salas e Laboratórios
+      <i class="fas fa-cog fa-2x"></i> Gerenciamento de Salas e Laboratórios
     </h1>
-    <div class="menu-toggle" @click="toggleSidebar">
-      <i class="fas fa-bars"></i>
+    <div class="menu-toggle" @click="$emit('toggleSidebar')">
+      <i class="fas fa-bars fa-2x"></i>
     </div>
   </header>
 </template>
 
 <script>
 export default {
-  methods: {
-    toggleSidebar() {
-      this.$emit('toggle-sidebar');
-    }
-  }
+  methods: {},
 };
 </script>
 
 <style scoped>
-/* Adicione estilos para o cabeçalho aqui */
 header {
   display: flex;
   justify-content: space-between;
@@ -33,13 +27,12 @@ header {
 header h1 {
   display: flex;
   align-items: center;
-  font-size: 1.5rem;
   margin: 0;
 }
 header h1 i {
   margin-right: 10px;
 }
-.menu-toggle {
+header .menu-toggle {
   cursor: pointer;
 }
 </style>
