61f5e6358f2c5a9d3c1ff4becf67f14592284273
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-10-25 17:40:12 -0300

    feat: add MainLayout.vue

---
+++ gerenciamentoSalas/src/layouts/MainLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="main-layout">
+    <Header @toggleSidebar="toggleSidebar" />
+    <Sidebar :sidebarVisible="sidebarVisible" @closeSidebar="toggleSidebar" />
+    <main>
+      <router-view></router-view>
+      <!-- Aqui as páginas serão renderizadas -->
+    </main>
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import Sidebar from "../components/Sidebar.vue";
+
+export default {
+  components: {
+    Header,
+    Sidebar,
+  },
+  data() {
+    return {
+      sidebarVisible: false, // Controle da visibilidade da Sidebar
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.sidebarVisible = !this.sidebarVisible; // Alterna a visibilidade da Sidebar
+    },
+    closeSidebar() {
+      this.sidebarVisible = false; // Fecha a sidebar
+    }
+  },
+};
+</script>
+
+<style scoped>
+.main-layout {
+  display: flex;
+  flex-direction: column;
+}
+</style>
