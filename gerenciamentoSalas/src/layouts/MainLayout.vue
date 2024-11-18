3854d04661bb612116b0b5b7712baa485bcbc53d
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-11-18 19:50:07 -0300

    fix: Resolvendo conflitos no MainLayout.vue

--- gerenciamentoSalas/src/layouts/MainLayout.vue
+++ gerenciamentoSalas/src/layouts/MainLayout.vue
@@ -6,41 +6,20 @@
       <router-view></router-view>
       <!-- Aqui as páginas serão renderizadas -->
     </main>
-<<<<<<< HEAD
-<<<<<<< HEAD
     <Footer />
-=======
->>>>>>> bb3adb8 (feat: add MainLayout.vue)
-=======
-    <Footer />
->>>>>>> e1d2f6c (feat: update MainLayout.vue)
   </div>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
 import Sidebar from "../components/Sidebar.vue";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Footer from '../components/Footer.vue';
-=======
->>>>>>> bb3adb8 (feat: add MainLayout.vue)
-=======
-import Footer from '../components/Footer.vue';
->>>>>>> e1d2f6c (feat: update MainLayout.vue)
+import Footer from "../components/Footer.vue";
 
 export default {
   components: {
     Header,
     Sidebar,
-<<<<<<< HEAD
-<<<<<<< HEAD
     Footer,
-=======
->>>>>>> bb3adb8 (feat: add MainLayout.vue)
-=======
-    Footer,
->>>>>>> e1d2f6c (feat: update MainLayout.vue)
   },
   data() {
     return {
@@ -53,7 +32,7 @@ export default {
     },
     closeSidebar() {
       this.sidebarVisible = false; // Fecha a sidebar
-    }
+    },
   },
 };
 </script>
@@ -62,10 +41,6 @@ export default {
 .main-layout {
   display: flex;
   flex-direction: column;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f119fd6 (feat: Update component MainLayout.vue)
   min-height: 100vh;
   /* Define o mínimo como altura total da tela */
 }
@@ -73,10 +48,5 @@ export default {
 main {
   flex-grow: 1;
   /* Faz com que o conteúdo principal ocupe o espaço disponível */
-<<<<<<< HEAD
-=======
->>>>>>> bb3adb8 (feat: add MainLayout.vue)
-=======
->>>>>>> f119fd6 (feat: Update component MainLayout.vue)
 }
 </style>
