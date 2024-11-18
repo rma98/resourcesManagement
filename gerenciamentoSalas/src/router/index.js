d8fa0a163f0db053be3744f536343676f2cb76d7
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-11-18 16:13:17 -0300

    chore: Update router index.js

--- gerenciamentoSalas/src/router/index.js
+++ gerenciamentoSalas/src/router/index.js
@@ -1,63 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../views/Login.vue'; // Importe o componente Login
-import Register from '../views/Register.vue';
+import { useStore } from 'vuex'; // Importa o Vuex para verificar o login
+
+import MainLayout from "../layouts/MainLayout.vue";
 import Home from '../views/Home.vue';
-import Adicionar from '../views/Adicionar.vue';
-import RecursoAdicional from '../views/RecursoAdicional.vue';
-import Salas from '../views/Salas.vue';
-import Laboratorios from '../views/Laboratorios.vue';
-import Reserva from '../views/Reserva.vue';
+import Register from '../views/Register.vue';
+import Login from '../views/Login.vue';
 
 const routes = [
-    { 
-        path: '/login', // Define a rota para o componente de login
-        name: 'Login', 
-        component: Login 
-    },
-    { 
-        path: '/register',
-        name: 'Register', 
-        component: Register 
-    },
-    { 
-        path: '/adicionar', 
-        name: 'Adicionar', 
-        component: Adicionar 
-    },
-    { 
-        path: '/recursos-adicionais', 
-        name: 'RecursoAdicional', 
-        component: RecursoAdicional 
-    },
-    {
-        path: '/salas',
-        name: 'Salas',
-        component: Salas
-    },
-    {
-        path: '/laboratorios',
-        name: 'Laboratorios',
-        component: Laboratorios
-    },
-    {
-        path: '/reserva',
-        name: 'Reserva',
-        component: Reserva
-    },
     {
         path: '/',
-        name: 'Home',
-        component: Home, // Defina a rota para Home.vue
+        component: MainLayout,
+        children: [
+            { path: '', name: 'Home', component: Home, },
+            { path: '/register', name: 'Register', component: Register },
+            { path: '/login', name: 'Login', component: Login },
+        ],
     },
-    {
-        path: '/:pathMatch(.*)*',
-        redirect: '/'
-    },
+    { path: '/:pathMatch(.*)*', redirect: '/' }
 ];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
+});
+
+// Guard de Navegação Global
+router.beforeEach((to, from, next) => {
+    const store = useStore(); // Acessa a store do Vuex para verificar o login
+
+    // Verifica se a rota requer autenticação
+    if (to.meta.requiresAuth && !store.state.user.name) {
+        // Redireciona para o login se o usuário não estiver logado
+        next({ name: 'Login' });
+    } else {
+        next(); // Permite a navegação
+    }
 });
 
 export default router;
