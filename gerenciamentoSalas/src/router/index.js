8bfa38e5e18896d7be2a4ebd2ff713a4ab4153ae
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-11-18 16:38:20 -0300

    feat: Update router index.js

--- gerenciamentoSalas/src/router/index.js
+++ gerenciamentoSalas/src/router/index.js
@@ -5,6 +5,8 @@ import MainLayout from "../layouts/MainLayout.vue";
 import Home from '../views/Home.vue';
 import Register from '../views/Register.vue';
 import Login from '../views/Login.vue';
+import AddRecurso from '../views/AddRecurso.vue';
+import EditRecurso from '../views/EditRecurso.vue';
 
 const routes = [
     {
@@ -14,8 +16,10 @@ const routes = [
             { path: '', name: 'Home', component: Home, },
             { path: '/register', name: 'Register', component: Register },
             { path: '/login', name: 'Login', component: Login },
+            { path: '/add-recurso', name: 'AddRecurso', component: AddRecurso, meta: { requiresAuth: true }, },
         ],
     },
+    { path: '/editar-recurso/:tipo/:id', name: 'EditRecurso', component: EditRecurso, meta: { requiresAuth: true }, },
     { path: '/:pathMatch(.*)*', redirect: '/' }
 ];
 
