7d258ff69f28e5f36afbc5af90ff816a2f30d285
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-11-18 16:13:17 -0300

    chore: Update router index.js

--- gerenciamentoSalas/src/router/index.js
+++ gerenciamentoSalas/src/router/index.js
@@ -5,8 +5,11 @@ import MainLayout from "../layouts/MainLayout.vue";
 import Home from '../views/Home.vue';
 import Register from '../views/Register.vue';
 import Login from '../views/Login.vue';
+<<<<<<< HEAD
 import AddRecurso from '../views/AddRecurso.vue';
 import EditRecurso from '../views/EditRecurso.vue';
+=======
+>>>>>>> 30dc309 (chore: Update router index.js)
 
 const routes = [
     {
@@ -16,10 +19,15 @@ const routes = [
             { path: '', name: 'Home', component: Home, },
             { path: '/register', name: 'Register', component: Register },
             { path: '/login', name: 'Login', component: Login },
+<<<<<<< HEAD
             { path: '/add-recurso', name: 'AddRecurso', component: AddRecurso, meta: { requiresAuth: true }, },
         ],
     },
     { path: '/editar-recurso/:tipo/:id', name: 'EditRecurso', component: EditRecurso, meta: { requiresAuth: true }, },
+=======
+        ],
+    },
+>>>>>>> 30dc309 (chore: Update router index.js)
     { path: '/:pathMatch(.*)*', redirect: '/' }
 ];
 
