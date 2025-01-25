2c9dcd400c2e9fc37f8f4d5bb7bb12d5a58e7960
Hiany Gabriely Ferreira da Silva <76077312+HianySilva@users.noreply.github.com>
2025-01-24 22:28:56 -0300

    feat: Update main.js

--- gerenciamentoSalas/src/main.js
+++ gerenciamentoSalas/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import './assets/styles/cardStyles.css';
 import './assets/styles/global.css';
 
 import '@fortawesome/fontawesome-free/css/all.css';
