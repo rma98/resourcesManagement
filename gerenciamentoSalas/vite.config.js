93776d6f718f285f79a83f9367e38c2679afa10f
Hiany Gabriely Ferreira da Silva <76077312+HianySilva@users.noreply.github.com>
2025-01-24 23:00:38 -0300

    Atualizar arquivos iniciais do projeto

--- gerenciamentoSalas/vite.config.js
+++ gerenciamentoSalas/vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/resourcesManagement/' // Nome do repositório no GitHub
 })
